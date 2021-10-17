a70f58658f8c0074aa89338763928ef49c639b86
Alok Chauhan <alokc@qti.qualcomm.com>
2021-10-17 23:02:58 +0530

    ARM: dts: msm: Add camera device tree for Cape
    Add device tree changes for memory, regulators,
    clock and interrupt information for CPAS, LX7,
    CDM, TPG, VFE, CSID, IPE, BPS, JPEG, VFE-LITE
    and CSID-LITE nodes. Update SMMU Node. Add new
    SFE Nodes.
    
    CRs-Fixed: 3063982
    Change-Id: I631d77eb66af25d37e030768133e5a9e7d83861d

--- config/waipio.mk
+++ config/waipio.mk
@@ -5,3 +5,4 @@ dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
 								waipio-camera-sensor-qrd.dtbo
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
+dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
